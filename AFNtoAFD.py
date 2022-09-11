1f9cf47f435d9b1a80110b245a37669426202ce1
unknown <josemarianoreyesh02@gmail.com>
2022-09-11 09:18:21 -0600

    añadir epsilon a simbolos/alfabeto

--- AFNtoAFD.py
+++ AFNtoAFD.py
@@ -6,11 +6,14 @@ import pandas as pd
 
 class AFNtoAFD():
     def __init__(self, estados_afn, transiciones_afn, estado_inicial_afn, estado_final_afn, simbolos_afn):
+        # variables necesarias para poder pasar de un afn a un afd
         self.estados_afn = estados_afn
         self.transiciones_afn = transiciones_afn
         self.estado_inicial_afn = estado_inicial_afn
         self.estado_final_afn = estado_final_afn
         self.simbolos_afn = simbolos_afn
+        # añadimos el ε como parte de los símbolos/alfabeto
+        self.simbolos_afn.append('ε')
 
     def conversion(self):
         print("\nAcá va el metodo de conversion de afn a afd")
