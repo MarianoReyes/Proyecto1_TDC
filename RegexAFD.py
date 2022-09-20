f0e3c2ed0669aa288c46bf10d487b3a0d9685d1f
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-09-20 01:44:16 -0600

    ya se hace el regex

--- RegexAFD.py
+++ RegexAFD.py
@@ -28,11 +28,83 @@ def regex(expresion, isAFD=False):
                     i=i+1
                 else:
                     first.pop()
-                
-                
+            else:
+                regex_list.append(expresion[i]) #si no es el cierre del parentesis lo agrega a la lista
+            
+            i=i+1 #cambio de i 
+            
+    #Segundo caso especial
+    if expresion.find(SpecialCases['null_check_group']) != -1:
+        while i < len(expresion):
+            if expresion[i] == '(':
+                first.append(i)                        
+
+            if expresion[i] == ')':
+                regex_list.append(expresion[i])  # pos actual
+                if expresion[i + 1] == '?':     #si la siguiente posicion es ?
+                    last_index = i + 1
+                    regex_list.append('|')
+                    regex_list.append('ε')
+                    regex_list.append(')')
+                    regex_list.insert(first[-1], '(')
+                    i = i + 1
+                else:
+                    first.pop()
+
+            else:
+                regex_list.append(expresion[i])
+            i += 1
+
+        expresion = ''.join(regex_list)
+
+    resultado = expresion
     
+     #Tercer caso especial
+    if expresion.find(SpecialCases['positive_closure']) != -1:
+        while resultado.find(SpecialCases['positive_closure']) != -1:
+            i = resultado.find('+')
+            symbol = resultado[i - 1]
+
+            resultado = resultado.replace(symbol + '+', '(' + symbol + '*' + symbol + ')')
+
+    #Cuarto caso especial
+    if expresion.find(SpecialCases['null_check']) != -1:
+        while resultado.find(SpecialCases['null_check']) != -1:
+            i = resultado.find('?')
+            symbol = resultado[i - 1]
+
+            resultado = resultado.replace(symbol + '?', '(' + symbol + '|' + 'ε' + ')')
+                
+                
+    if(isAFD == True):
+        final_regex = '(' + final_regex + ')#'
+        
     return concatenacion(resultado)
     
 def concatenacion(regex):
+    valid_operators = ['(','*','|','?','+']
+    encadenado = ''
+    i = 0
     
-    return(encadenado)
+    while i < len(regex):
+        if i+1 >= len(regex):
+            encadenado += regex[-1]
+            break
+        if regex[i] == '*' and regex[i+1] != ')' and not (regex[i+1] in valid_operators):
+            encadenado += regex[i]+'.'
+        elif regex[i] == '*' and regex[i+1] == '(':
+            encadenado += regex[i]+'.'
+        elif regex[i] == '?' and regex[i+1] != ')' and not (regex[i+1] in valid_operators):
+            encadenado += regex[i]+'.'
+        elif regex[i] == '?' and regex[i+1] == '(':
+            encadenado += regex[i]+'.'
+        elif not (regex[i] in valid_operators) and regex[i+1] == ')':
+            encadenado += regex[i]
+        elif (not (regex[i] in valid_operators) and not (regex[i+1] in valid_operators)) or (not (regex[i] in valid_operators) and (regex[i+1] == '(')):
+            encadenado += regex[i]+'.'
+        else:
+            encadenado += regex[i]
+        i += 1
+        
+    return encadenado
+    
