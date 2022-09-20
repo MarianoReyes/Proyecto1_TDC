d6b82c9cabcc92fb69e444f4792b87ab00c20c8a
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-09-19 21:03:48 -0600

    changes

---
+++ RegexAFD.py
@@ -0,0 +1,19 @@
+
+def regex(expresion, isAFD=False):
+    first=[]
+    regex_list=[]
+    last_index = 0
+    i=0
+    
+    SpecialCases = {
+        'positive_closure_group':')+',
+        'null_check_group':')?',
+        'positive_closure':'+',
+        'null_check':'?',
+    }
+    
+    return concatenacion(resultado)
+    
+def concatenacion(regex):
+    
+    return(encadenado)
